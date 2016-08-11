182f808a8edd4fc0920e989d4ea9410d15bc3b26
mojopon <tyamuro@gmail.com>
2016-08-11 15:56:43 +0900

    add feature to find HoldHandTarget
    add feature to find HoldHandTarget

---
+++ Assets/Scripts/HoldHand.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldHand : MonoBehaviour
+{
+    public float searchRadius = 1f;
+
+    void FixedUpdate()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, searchRadius);
+
+        foreach(var col in hitColliders)
+        {
+            var target = col.GetComponent<HoldHandTarget>();
+
+            if(target)
+            {
+                FaceToTarget(target);
+            }
+        }
+    }
+
+    void FaceToTarget(HoldHandTarget target)
+    {
+        transform.LookAt(target.transform);
+    }
+}
